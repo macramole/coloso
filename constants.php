a2957146680f4d6ed501943c81adf22156d3f52c
macramole <leandrogarber@gmail.com>
2017-02-07 16:25:52 -0300

    cola de animaciones en php

--- constants.php
+++ constants.php
@@ -1,4 +1,5 @@
 <?php
 
 $UPLOAD_PATH = "uploads/";
+$DONE_PATH = "uploads/done/";
 $PROCESSING_FILE = $UPLOAD_PATH . "procesando.txt";
--- getAnimation.php
+++ getAnimation.php
@@ -13,4 +13,12 @@ foreach ( scandir( $UPLOAD_PATH ) as $file ) {
 
 if ( $fileToOpen != null ) {
     echo file_get_contents($UPLOAD_PATH . $fileToOpen);
+    rename( $UPLOAD_PATH . $fileToOpen, $DONE_PATH . $fileToOpen );
+    // if ( isset($_GET['do']) ) {
+    //     // print "doing...";
+    // }
+
+
+} else {
+    echo "nada";
 }
